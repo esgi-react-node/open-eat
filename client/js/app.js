2da9059e8607f3768ed80f482afe77f49e11c79c
Amin NAIRI <nairi_a16@myges.fr>
2020-07-11 23:35:24 +0200

    :sparkles: parallel loading of external libraries

--- client/js/app.js
+++ client/js/app.js
@@ -3,7 +3,6 @@
 import { h, render, Component } from 'preact';
 import { useState } from 'preact/hooks';
 import Router from 'preact-router';
-import '../css/app.css';
 import checkConnectivity from './lib/network.js';
 import { checkConnectedUser } from './lib/sdkGoogle.js';
 import { initFirebase } from "./lib/firebase";
@@ -15,10 +14,6 @@ import { ShowComments } from './components/ShowComments';
 import { AddComment } from './components/AddComment';
 import { Restaurant } from './context/Restaurant';
 import { User } from './context/User';
-
-initFirebase();
-checkConnectivity();
-checkConnectedUser();
 
 class App extends Component {
     render() {
@@ -44,4 +39,51 @@ class App extends Component {
     }
 }
 
-render(<App />, document.body);
+function loadScript(url) {
+    return new Promise((resolve) => {
+        const script = document.createElement("script");
+
+        script.setAttribute("async", true);
+        script.setAttribute("src", url);
+
+        document.body.appendChild(script);
+
+        script.addEventListener("load", resolve);
+    });
+}
+
+function loadStylesheet(url) {
+    return new Promise((resolve) => {
+        const stylesheet = document.createElement("link");
+
+        stylesheet.setAttribute("rel", "stylesheet");
+        stylesheet.setAttribute("href", url);
+
+        document.body.appendChild(stylesheet);
+
+        stylesheet.addEventListener("load", resolve);
+    });
+}
+
+window.addEventListener("load", () => {
+    Promise.all([
+        loadStylesheet("/main.css"),
+        loadStylesheet("https://fonts.googleapis.com/icon?family=Material+Icons"),
+        loadScript("https://cdnjs.cloudflare.com/ajax/libs/firebase/7.16.0/firebase-app.min.js")
+    ]).then(() => {
+        initFirebase();
+        checkConnectivity();
+        checkConnectedUser();
+
+        render(<App />, document.body);
+    });
+
+
+    if (navigator?.serviceWorker) {
+        navigator.serviceWorker.register("service-worker.js").then(() => {
+            console.log("Successfully registered the Service Worker.");
+        }).catch(() => {
+            console.error("Failed to register the service worker");
+        });
+    }
+});
