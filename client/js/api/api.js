d66f1e6d629804e3e5c52bb5acf5dfb1498ecc0c
Amin NAIRI <nairi_a16@myges.fr>
2020-07-12 12:20:46 +0200

    :sparkles: added conditionnal url to cloud functions

--- client/js/api/api.js
+++ client/js/api/api.js
@@ -1,4 +1,8 @@
 export const fetchApi = (url, urlMethod = 'GET', urlBody = {}) => {
+    console.group("DEBUG NODE_ENV");
+    console.log(JSON.stringify(process.env.NODE_ENV));
+    console.groupEnd();
+
     let options = {
         method: urlMethod,
         headers: {
@@ -10,7 +14,8 @@ export const fetchApi = (url, urlMethod = 'GET', urlBody = {}) => {
         options.body = JSON.stringify(urlBody);
     }
 
-    return fetch(`http://0.0.0.0:9005/openeat-a325a/us-central1/api/${url}`, options).then(response => response.json())
-    .then(response => response)
-    .catch(error => console.error(error));
+    return fetch(fromCloundFunction, options)
+        .then(response => response.json())
+        .then(response => response)
+        .catch(error => console.error(error));
 }
